1886f8c07a70b5b7d1ad78089df3c3fd8e9f70a0
Nanai Jua <me@n7i.dev>
2022-07-18 02:03:34 +0900

    🚧 add `cli.ts` for cli usage.

---
+++ cli.ts
@@ -0,0 +1,69 @@
+const std = {
+  flags: await import("https://deno.land/std/flags/mod.ts"),
+  path: await import("https://deno.land/std/path/mod.ts"),
+};
+
+import { Watcher, WatchEvent } from "./watcher.ts";
+
+const main = (args: string[]) => {
+  const { _: paths, t, s, a } = std.flags.parse(args);
+
+  let run: (arg: string) => void;
+  if (typeof t === "string" && s === undefined) {
+    run = (arg: string) =>
+      void Deno.run({ cmd: ["deno", "task", ...t.split(/\s+/), arg] });
+  } else if (typeof s === "string" && t === undefined) {
+    run = (arg: string) => void Deno.run({ cmd: [...s.split(/\s+/), arg] });
+  } else {
+    return console.log("needs to provide action");
+  }
+
+  if (a !== undefined && typeof a !== "boolean") {
+    return console.log("-a is boolean flag");
+  }
+  const all = a;
+
+  const watchers = (paths as unknown[])
+    .filter((e) => typeof e === "string")
+    .map((e) => e as string)
+    .map((s) => new Watcher(s));
+
+  const prehandle = (e: Event) => {
+    if (!(e instanceof WatchEvent)) return;
+
+    switch (e.content.type) {
+      case "touch":
+      case "new":
+      case "modify":
+        return handle(e.content.at);
+
+      case "move":
+        return handle(e.content.to);
+    }
+  };
+
+  const handle = (path: string) => {
+    if (!all && path.endsWith("~")) return;
+
+    const hidden =
+      path.split(std.path.sep).find((s) => s.startsWith(".")) === undefined;
+    if (!all && !hidden) return;
+
+    run(path);
+  };
+
+  for (const w of watchers) {
+    w.addEventListener("touch", prehandle);
+    w.addEventListener("new", prehandle);
+    w.addEventListener("move", prehandle);
+    w.addEventListener("modify", prehandle);
+  }
+
+  return Promise.all(watchers.map((w) => w.watch()));
+};
+
+export default main;
+
+if (import.meta.main) {
+  await main(Deno.args);
+}
