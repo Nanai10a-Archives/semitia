44f4d825ed0986a016187642f0605e2a532288b5
Nanai Jua <me@n7i.dev>
2022-07-18 05:57:28 +0900

    🚧 replace cli handlings to use cliffy.

--- cli.ts
+++ cli.ts
@@ -1,53 +1,65 @@
-import { Std } from "./deps.ts";
+import { Cliffy, Std } from "./deps.ts";
 
 import { Watcher, WatchEvent } from "./watcher.ts";
+import { VERSION } from "./version.ts";
 
-const main = (args: string[]) => {
-  const { _: paths, t, s, a } = Std.flags.parse(args);
+const main = async (args: string[]) => {
+  const result = await new Cliffy.Command()
+    .name("semitia")
+    .version(VERSION)
+    .description("thin wrapper of Deno.watchFs.")
+    .option("-t, --task <task:string>", "executes deno task.", {
+      collect: true,
+      conflicts: ["shell"],
+      required: true,
+    })
+    .option("-s, --shell <cmd:string>", "executes shell command.", {
+      collect: true,
+      conflicts: ["task"],
+      required: true,
+    })
+    .option("-a, --all", "unignore `.dot` and `temp~` files.")
+    .arguments("<dirs...:string>")
+    .parse(args);
 
-  let run: (arg: string) => void;
-  if (typeof t === "string" && s === undefined) {
-    run = (arg: string) =>
-      void Deno.run({ cmd: ["deno", "task", ...t.split(/\s+/), arg] });
-  } else if (typeof s === "string" && t === undefined) {
-    run = (arg: string) => void Deno.run({ cmd: [...s.split(/\s+/), arg] });
+  const paths = [...result.args[0], ...result.literal];
+  const opts = result.options;
+
+  const execute = [] as string[];
+
+  if (opts.shell) {
+    execute.push(...opts.shell);
+  } else if (opts.task) {
+    execute.push("deno", "task", ...opts.task);
   } else {
-    return console.log("needs to provide action");
+    throw new Error("non-provided executions.");
   }
 
-  if (a !== undefined && typeof a !== "boolean") {
-    return console.log("-a is boolean flag");
-  }
-  const all = a;
+  const prehandle = (event: Event) => {
+    if (!(event instanceof WatchEvent)) return;
 
-  const watchers = (paths as unknown[])
-    .filter((e) => typeof e === "string")
-    .map((e) => e as string)
-    .map((s) => new Watcher(s));
-
-  const prehandle = (e: Event) => {
-    if (!(e instanceof WatchEvent)) return;
-
-    switch (e.content.type) {
+    switch (event.content.type) {
       case "touch":
       case "new":
       case "modify":
-        return handle(e.content.at);
+        return handle(event.content.at);
 
       case "move":
-        return handle(e.content.to);
+        return handle(event.content.to);
     }
   };
 
   const handle = (path: string) => {
-    if (!all && path.endsWith("~")) return;
+    if (!opts.all && path.endsWith("~")) return;
 
     const hidden =
       path.split(Std.path.sep).find((s) => s.startsWith(".")) === undefined;
-    if (!all && !hidden) return;
+    if (!opts.all && !hidden) return;
 
-    run(path);
+    Deno.run({ cmd: [...execute, path] });
   };
+
+  const watchers = paths.map((s) => new Watcher(s));
 
   for (const w of watchers) {
     w.addEventListener("touch", prehandle);
