dad0425a0d65560264116aebd0f9ad62888e7b4b
Nanai Jua <me@n7i.dev>
2022-07-20 06:15:38 +0900

    ➕ add `std/testing/asserts` .

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
 import * as flags from "https://deno.land/std/flags/mod.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
-const Std = { flags, path };
+import * as asserts from "https://deno.land/std/testing/asserts.ts";
+const Std = { flags, path, testing: { asserts } };
 
 import * as Cliffy from "https://deno.land/x/cliffy/mod.ts";
 
