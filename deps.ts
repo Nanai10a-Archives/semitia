ffd77372544606de9b77324c1d998700654281dd
Nanai Jua <me@n7i.dev>
2022-07-24 06:33:21 +0900

    ➕ add `std/fs` .

--- deps.ts
+++ deps.ts
@@ -1,7 +1,8 @@
 import * as flags from "https://deno.land/std/flags/mod.ts";
+import * as fs from "https://deno.land/std/fs/mod.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
 import * as asserts from "https://deno.land/std/testing/asserts.ts";
-const Std = { flags, path, testing: { asserts } };
+const Std = { flags, fs, path, testing: { asserts } };
 
 import * as Cliffy from "https://deno.land/x/cliffy/mod.ts";
 
