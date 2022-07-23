74ad3ad20cfa8e9030cd47568a5ce2cf0a57d1bf
Nanai Jua <me@n7i.dev>
2022-07-24 06:34:46 +0900

    💚 add tests.

--- watcher.test.ts
+++ watcher.test.ts
@@ -31,3 +31,83 @@ Deno.test("event: touch (c)", async () => {
 
   assertEquals(event.content, { type: "touch", at });
 });
+
+Deno.test("event: touch (cm)", async () => {
+  const dir = await mktmp();
+
+  const w = new Watcher(dir);
+  const eventPromise = asWEPromise(w, "touch");
+
+  w.watch();
+
+  const at = Std.path.join(dir, "touch");
+  await Deno.writeTextFile(at, "touch");
+
+  const event = await eventPromise;
+
+  w.abort();
+  await rmtmp(dir);
+
+  assertEquals(event.content, { type: "touch", at });
+});
+
+Deno.test("event: move (mm)", async () => {
+  const dir = await mktmp();
+
+  const w = new Watcher(dir);
+  const eventPromise = asWEPromise(w, "move");
+
+  w.watch();
+
+  const from = Std.path.join(dir, "move-from");
+  const to = Std.path.join(dir, "move-to");
+  await Deno.writeTextFile(from, "");
+  await Std.fs.move(from, to);
+
+  const event = await eventPromise;
+
+  w.abort();
+  await rmtmp(dir);
+
+  assertEquals(event.content, { type: "move", from, to });
+});
+
+Deno.test("event: modify (m)", async () => {
+  const dir = await mktmp();
+
+  const w = new Watcher(dir);
+  const eventPromise = asWEPromise(w, "modify");
+
+  w.watch();
+
+  const at = Std.path.join(dir, "modify");
+  await Deno.writeTextFile(at, "");
+  await Deno.writeTextFile(at, "modify");
+
+  const event = await eventPromise;
+
+  w.abort();
+  await rmtmp(dir);
+
+  assertEquals(event.content, { type: "modify", at });
+});
+
+Deno.test("event: remove (r)", async () => {
+  const dir = await mktmp();
+
+  const w = new Watcher(dir);
+  const eventPromise = asWEPromise(w, "remove");
+
+  w.watch();
+
+  const at = Std.path.join(dir, "remove");
+  await Deno.writeTextFile(at, "");
+  await Deno.remove(at);
+
+  const event = await eventPromise;
+
+  w.abort();
+  await rmtmp(dir);
+
+  assertEquals(event.content, { type: "remove", at });
+});
