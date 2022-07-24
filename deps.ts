f1045b36b7952057a26d995ff5f32238e2912bfc
Nanai Jua <me@n7i.dev>
2022-07-24 10:50:50 +0900

    ➕ add `std/async` .

--- deps.ts
+++ deps.ts
@@ -1,8 +1,9 @@
+import * as async from "https://deno.land/std/async/mod.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
 import * as fs from "https://deno.land/std/fs/mod.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
 import * as asserts from "https://deno.land/std/testing/asserts.ts";
-const Std = { flags, fs, path, testing: { asserts } };
+const Std = { async, flags, fs, path, testing: { asserts } };
 
 import * as Cliffy from "https://deno.land/x/cliffy/mod.ts";
 
